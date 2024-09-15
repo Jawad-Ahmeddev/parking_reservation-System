2f5f480b673a8556cf656cab7ecfd132b77a4e95
Jawad Ahmed Khan <140721252+Jawad-Ahmeddev@users.noreply.github.com>
2024-09-15 15:14:45 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
